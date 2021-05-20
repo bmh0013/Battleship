a263c1d5d6b66e7c2592fafe684a9c5b1a779530
Brandon Harden <bmh0013@gmail.com>
2021-05-20 15:27:19 -0500

    configuration done

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const config = {
+  entry: './client/src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'client/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+
+    ]
+  },
+
+};
+
+module.exports = config;
